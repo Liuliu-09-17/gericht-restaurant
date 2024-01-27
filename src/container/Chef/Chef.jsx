c5bfc89289c8fab7b65db0f3a30b5caf9ac85889
Yang Liu <liuyy917@gmail.com>
2024-01-26 21:48:35 -0500

    Initial commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa
+          nesciunt odit totam optio ut delectus quam blanditiis porro commodi
+          mollitia at, labore, non, neque consequuntur obcaecati quia suscipit
+          soluta.
+        </p>
+
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+          <img src={images.sign} alt="sign" />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
